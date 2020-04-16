774fc746d97783bae37a0f35c12f98f16b2862e1
nikhil <nikhilkumar516@gmail.com>
2020-04-15 22:22:32 -0400

    Added app JSONField scheme validation

--- toil_orchestrator/serializers.py
+++ toil_orchestrator/serializers.py
@@ -1,6 +1,58 @@
 from rest_framework import serializers
 from .models import Job, CommandLineToolJob, Status
+from django.core.exceptions import ValidationError
+from drf_yasg import openapi
 
+
+class AppField(serializers.JSONField):
+
+    def validate_app(app_data):
+        if 'github' not in app_data:
+            if 'base64' in app_data:
+                raise ValidationError("base64 is not supported", code=501)
+            elif 'app' in app_data:
+                raise ValidationError("app is not supported", code=501)
+            else:
+                raise ValidationError("Invalid app reference type", code=400)
+
+    default_validators = [validate_app]
+
+
+    class Meta:
+        swagger_schema_fields = {
+            "type": openapi.TYPE_OBJECT,
+            "title": "App",
+            "properties": {
+                "github": openapi.Schema(
+                    title="github",
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        "repository": openapi.Schema(
+                            title="repository",
+                            type=openapi.TYPE_STRING,
+                        ),
+                        "entrypoint": openapi.Schema(
+                            title="entrypoint",
+                            type=openapi.TYPE_STRING,
+                        ),
+                        "version": openapi.Schema(
+                            title="version",
+                            type=openapi.TYPE_STRING,
+                        ),
+
+                    },
+                    required=["repository", "entrypoint"]
+                ),
+                "base64": openapi.Schema(
+                    title="base64",
+                    type=openapi.TYPE_STRING,
+                ),
+                "app": openapi.Schema(
+                    title="app",
+                    type=openapi.TYPE_STRING,
+                )
+            }
+         }
 
 class JobSerializer(serializers.ModelSerializer):
     status = serializers.SerializerMethodField()
