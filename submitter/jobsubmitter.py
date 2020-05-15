7201550db1330aeafb6d70afb2d68300d8cf67aa
Adrian Fraiha <aef@fastmail.com>
2020-05-15 12:38:19 -0400

    Update jobsubmitter.py

--- submitter/jobsubmitter.py
+++ submitter/jobsubmitter.py
@@ -110,7 +110,7 @@ class JobSubmitter(object):
             Start ACCESS-specific code
             In addition to different arguments and required bins, ACCESS requires a specific version of Toil that prevents the following PATH from recursively appending itself on every job, causing "OSExit Too many argument".
             """
-            path = "PATH=:/juno/work/ci/access-pipelines/miniconda3/envs/ACCESS_1.3.26/bin:/juno/work/ci/access-pipelines/toil-msk-3.21.1-MSK-rc1/bin:{}".format(os.environ.get('PATH'))
+            path = "PATH=/juno/work/ci/access-pipelines/toil-msk-3.21.1-MSK-rc1/bin:/juno/work/ci/access-pipelines/miniconda3/envs/ACCESS_1.3.26/bin:{}".format(os.environ.get('PATH'))
             command_line = [path, 'toil-cwl-runner', '--logFile', 'toil_log.log', '--batchSystem','lsf','--disable-user-provenance','--logLevel', 'DEBUG','--disable-host-provenance','--stats', '--debug', '--cleanWorkDir', 'always', '--disableCaching', '--preserve-environment', 'PATH', 'TMPDIR', 'TOIL_LSF_ARGS', 'SINGULARITY_PULLDIR', 'SINGULARITY_CACHEDIR', 'PWD', '_JAVA_OPTIONS', 'PYTHONPATH', 'TEMP', '--defaultMemory', '10G', '--realTimeLogging', '--jobStore', self.job_store_dir, '--tmpdir-prefix', self.job_tmp_dir, '--workDir', self.job_work_dir, '--outdir', self.job_outputs_dir]
             """
             End ACCESS-specific code
