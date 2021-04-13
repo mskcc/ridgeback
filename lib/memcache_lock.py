1ff98c86e3f138e021009034c350e2e139820423
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2021-04-13 13:09:11 -0400

    Refactor job status, tests, status_job_refactor

--- lib/memcache_lock.py
+++ lib/memcache_lock.py
@@ -1,8 +1,13 @@
 import time
 import functools
 from django.core.cache import cache
+from contextlib import contextmanager
 
-def memcache_lock(lock_id, expiration = 60 * 10):
+
+LOCK_EXPIRE = 60 * 10  # Lock expires in 10 minutes
+
+
+def memcache_lock(lock_id, expiration=60 * 10):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
@@ -17,3 +22,21 @@ def memcache_lock(lock_id, expiration = 60 * 10):
                         cache.delete(lock_id)
         return wrapper
     return decorator
+
+
+@contextmanager
+def memcache_task_lock(lock_id, oid):
+    timeout_at = time.monotonic() + LOCK_EXPIRE - 3
+    # cache.add fails if the key already exists
+    status = cache.add(lock_id, oid, LOCK_EXPIRE)
+    try:
+        yield status
+    finally:
+        # memcache delete is very slow, but we have to use it to take
+        # advantage of using add() for atomic locking
+        if time.monotonic() < timeout_at and status:
+            # don't release the lock if we exceeded the timeout
+            # to lessen the chance of releasing an expired lock
+            # owned by someone else
+            # also don't release the lock if we didn't acquire it
+            cache.delete(lock_id)
