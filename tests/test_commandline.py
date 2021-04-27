4dddb71b7d4eba233db10edebbdf1f4fe848feba
Nikhil <nikhilkumar516@gmail.com>
2021-04-27 09:26:33 -0400

    Fixed testing to download from s3

--- tests/test_commandline.py
+++ tests/test_commandline.py
@@ -1,23 +1,57 @@
+"""
+Tests for commandline status handling
+"""
+import os
+from shutil import unpack_archive, rmtree
+import requests
 from django.test import TestCase
 import toil
-import os
 from orchestrator.models import Job, Status, PipelineType, CommandLineToolJob
 from orchestrator.tasks import check_status_of_command_line_jobs
 
-SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-
 
 class TestToil(TestCase):
+    """
+    Test toil track functions
+    """
+
+    def download_toil_mock(self, toil_version):
+        """
+        Download TOIL mock data from s3
+        """
+        download_url = "https://toilmock.s3.amazonaws.com/toil_%s.tar.gz" % toil_version
+        download_path = "toil_%s.tar.gz" % toil_version
+        folder_path = "toil_%s" % toil_version
+        self.download_full_path = os.path.abspath(download_path)
+        self.mock_full_path = os.path.abspath(folder_path)
+        if not os.path.exists(self.download_full_path):
+            response = requests.get(download_url, stream=True)
+            if response.status_code == 200:
+                with open(self.download_full_path, "wb") as download:
+                    download.write(response.raw.read())
+            else:
+                raise Exception("Could not download TOIL mock data from: %s" % download_url)
+        if not os.path.exists(self.mock_full_path):
+            unpack_archive(self.download_full_path)
+
     def setUp(self):
         Job.objects.all().delete()
         self.toil_version = toil.version.baseVersion
         if self.toil_version not in ["3.21.0", "5.4.0a1"]:
             raise Exception("TOIL version: %s not supported" % self.toil_version)
-        mock_base_path = os.path.abspath(os.path.join(SCRIPT_DIR, "toil_mock"))
-        self.toil_mock = os.path.join(mock_base_path, "toil_%s" % self.toil_version)
+        self.download_toil_mock(self.toil_version)
+
+    def tearDown(self):
+        if os.path.exists(self.mock_full_path):
+            rmtree(self.mock_full_path)
+        if os.path.exists(self.download_full_path):
+            os.remove(self.download_full_path)
 
     def mock_track(self, run_type):
-        mock_data_path = os.path.join(self.toil_mock, run_type)
+        """
+        Mock track using TOIL snapshots
+        """
+        mock_data_path = os.path.join(self.mock_full_path, run_type)
         first_jobstore = os.path.join(mock_data_path, "0", "jobstore")
         first_work = os.path.join(mock_data_path, "0", "work")
         second_jobstore = os.path.join(mock_data_path, "1", "jobstore")
@@ -39,6 +73,9 @@ class TestToil(TestCase):
         check_status_of_command_line_jobs()
 
     def test_running(self):
+        """
+        Test if running and completed jobs are properly parsed
+        """
         self.mock_track("running")
         mock_num_completed = 0
         mock_num_running = 0
@@ -55,6 +92,9 @@ class TestToil(TestCase):
         self.assertEqual(num_completed, mock_num_completed)
 
     def test_failed(self):
+        """
+        Test if failed jobs are properly parsed
+        """
         self.mock_track("failed")
         mock_num_failed = 2
         num_failed = CommandLineToolJob.objects.filter(status=(Status.FAILED)).count()
