7b0bf172f373cdcb46ffc84e27550cafe9f2d128
Nikhil <nikhilkumar516@gmail.com>
2021-04-27 11:01:08 -0400

    Fixed lsf_client

--- batch_systems/lsf_client/lsf_client.py
+++ batch_systems/lsf_client/lsf_client.py
@@ -1,6 +1,6 @@
-'''
+"""
 Submit, monitor, and control LSF jobs
-'''
+"""
 import os
 import re
 import subprocess
@@ -9,6 +9,7 @@ import logging
 from random import randint
 from django.conf import settings
 from orchestrator.models import Status
+
 
 def get_LSF_job_group(job_id):
     """
@@ -20,16 +21,11 @@ def get_LSF_job_group(job_id):
     Returns:
         str: LSF job group
     """
-    return '/%s/%s' % (settings.RIDGEBACK_DEFAULT_QUEUE,job_id)
+    return "/%s/%s" % (settings.RIDGEBACK_DEFAULT_QUEUE, job_id)
 
-<<<<<<< HEAD
-class LSFClient():
 
-    """
-=======
 class LSFClient(object):
-    '''
->>>>>>> b423df9b40171c0508581a1425e3d54ad7911edd
+    """
     Client for LSF
 
     Attributes:
@@ -40,33 +36,22 @@ class LSFClient(object):
         """
         init function
         """
-        self.logger = logging.getLogger('LSF_client')
+        self.logger = logging.getLogger("LSF_client")
 
-<<<<<<< HEAD
-    def submit(self, command, job_args, stdout, job_id):
-        """
-=======
     def submit(self, command, job_args, stdout, env={}):
-        '''
->>>>>>> b423df9b40171c0508581a1425e3d54ad7911edd
+        """
         Submit command to LSF and store log in stdout
 
         Args:
             command (str): command to submit
             stdout (str): log file path
+            env (dict): the submitter env
 
         Returns:
             int: lsf job id
-<<<<<<< HEAD
         """
         job_group = get_LSF_job_group(job_id)
-        bsub_command = ['bsub', '-sla', settings.LSF_SLA,'-g',job_group,'-oo', stdout] + job_args
-        toil_lsf_args = '-sla %s -g %s %s' % (settings.LSF_SLA,job_group," ".join(job_args))
-=======
-        '''
-        bsub_command = ['bsub', '-sla', settings.LSF_SLA, '-oo', stdout] + job_args
->>>>>>> b423df9b40171c0508581a1425e3d54ad7911edd
-
+        bsub_command = ["bsub", "-sla", settings.LSF_SLA, "-g", job_group, "-oo", stdout] + job_args
         bsub_command.extend(command)
         current_env = os.environ
         for k, v in env.items():
@@ -76,8 +61,12 @@ class LSFClient(object):
                 current_env.pop(k)
         self.logger.debug("Running command: %s\nEnv: %s", bsub_command, current_env)
         process = subprocess.run(
-            bsub_command, check=True, stdout=subprocess.PIPE,
-            universal_newlines=True, env=current_env)
+            bsub_command,
+            check=True,
+            stdout=subprocess.PIPE,
+            universal_newlines=True,
+            env=current_env,
+        )
         return self._parse_procid(process.stdout)
 
     def abort(self, job_id):
@@ -92,11 +81,11 @@ class LSFClient(object):
         """
         self.logger.debug("Aborting LSF jobs for job %s", job_id)
         job_group = get_LSF_job_group(job_id)
-        bkill_command = ['bkill', '-g',job_group,'0']
+        bkill_command = ["bkill", "-g", job_group, "0"]
         try:
             subprocess.run(
-                bkill_command, check=True, stdout=subprocess.PIPE,
-                universal_newlines=True)
+                bkill_command, check=True, stdout=subprocess.PIPE, universal_newlines=True
+            )
         except subprocess.CalledProcessError:
             self.logger.error("Failed to abort LSF job %s", job_id)
             return False
@@ -115,10 +104,11 @@ class LSFClient(object):
         """
         self.logger.debug("Suspending LSF jobs for job %s", job_id)
         job_group = get_LSF_job_group(job_id)
-        bsub_command = ['bstop','-g',job_group,'0']
+        bsub_command = ["bstop", "-g", job_group, "0"]
         try:
-            subprocess.run(bsub_command, check=True, stdout=subprocess.PIPE,
-                                 universal_newlines=True)
+            subprocess.run(
+                bsub_command, check=True, stdout=subprocess.PIPE, universal_newlines=True
+            )
         except subprocess.CalledProcessError:
             self.logger.error("Failed to suspend LSF job %s", job_id)
             return False
@@ -137,10 +127,11 @@ class LSFClient(object):
         """
         self.logger.debug("Unsuspending LSF jobs for job %s", job_id)
         job_group = get_LSF_job_group(job_id)
-        bsub_command = ["bresume", '-g',job_group,'0']
+        bsub_command = ["bresume", "-g", job_group, "0"]
         try:
-            subprocess.run(bsub_command, check=True, stdout=subprocess.PIPE,
-                                 universal_newlines=True)
+            subprocess.run(
+                bsub_command, check=True, stdout=subprocess.PIPE, universal_newlines=True
+            )
         except subprocess.CalledProcessError:
             self.logger.error("Failed to unsuspend LSF job %s", job_id)
             return False
@@ -160,16 +151,16 @@ class LSFClient(object):
         bjobs_dict = None
         bjobs_records = None
         # Handle Cannot connect to LSF. Please wait ... type messages
-        dict_start = bjobs_output_str.find('{')
-        dict_end = bjobs_output_str.rfind('}')
+        dict_start = bjobs_output_str.find("{")
+        dict_end = bjobs_output_str.rfind("}")
         if dict_start != -1 and dict_end != -1:
-            bjobs_output = bjobs_output_str[dict_start:(dict_end+1)]
+            bjobs_output = bjobs_output_str[dict_start : (dict_end + 1)]
             try:
                 bjobs_dict = json.loads(bjobs_output)
             except json.decoder.JSONDecodeError:
                 self.logger.error("Could not parse bjobs output: %s", bjobs_output_str)
-            if 'RECORDS' in bjobs_dict:
-                bjobs_records = bjobs_dict['RECORDS']
+            if "RECORDS" in bjobs_dict:
+                bjobs_records = bjobs_dict["RECORDS"]
         if bjobs_records is None:
             self.logger.error("Could not find bjobs output json in: %s", bjobs_output_str)
 
@@ -186,7 +177,7 @@ class LSFClient(object):
             int: LSF id
         """
         self.logger.debug("LSF returned %s", stdout)
-        lsf_job_id_search = re.search('Job <(.*)> is submitted', stdout)
+        lsf_job_id_search = re.search("Job <(.*)> is submitted", stdout)
         if lsf_job_id_search:
             lsf_job_id = int(lsf_job_id_search[1])
             self.logger.debug("Got the job id: %s", lsf_job_id)
@@ -208,41 +199,36 @@ class LSFClient(object):
         Returns:
             tuple: (Ridgeback Status int, extra info)
         """
-        if process_status == 'DONE':
-            self.logger.debug(
-                "Job [%s] completed", external_job_id)
+        if process_status == "DONE":
+            self.logger.debug("Job [%s] completed", external_job_id)
             return Status.COMPLETED, None
-        if process_status == 'PEND':
+        if process_status == "PEND":
             pending_info = ""
-            if 'PEND_REASON' in process_output:
-                if process_output['PEND_REASON']:
-                    pending_info = process_output['PEND_REASON']
+            if "PEND_REASON" in process_output:
+                if process_output["PEND_REASON"]:
+                    pending_info = process_output["PEND_REASON"]
             self.logger.debug("Job [%s] pending with: %s", external_job_id, pending_info)
             return Status.PENDING, pending_info.strip()
-        if process_status == 'EXIT':
+        if process_status == "EXIT":
             exit_info = ""
-            if 'EXIT_CODE' in process_output:
-                if process_output['EXIT_CODE']:
-                    exit_code = process_output['EXIT_CODE']
+            if "EXIT_CODE" in process_output:
+                if process_output["EXIT_CODE"]:
+                    exit_code = process_output["EXIT_CODE"]
                     exit_info = "\nexit code: {}".format(exit_code)
-            if 'EXIT_REASON' in process_output:
-                if process_output['EXIT_REASON']:
-                    exit_reason = process_output['EXIT_REASON']
+            if "EXIT_REASON" in process_output:
+                if process_output["EXIT_REASON"]:
+                    exit_reason = process_output["EXIT_REASON"]
                     exit_info += "\nexit reason: {}".format(exit_reason)
-            self.logger.error(
-                "Job [%s] failed with: %s", external_job_id, exit_info)
+            self.logger.error("Job [%s] failed with: %s", external_job_id, exit_info)
             return Status.FAILED, exit_info.strip()
-        if process_status == 'RUN':
-            self.logger.debug(
-                "Job [%s] is running", external_job_id)
+        if process_status == "RUN":
+            self.logger.debug("Job [%s] is running", external_job_id)
             return Status.RUNNING, None
-        if process_status in {'PSUSP', 'USUSP', 'SSUSP'}:
-            self.logger.debug(
-                "Job [%s] is suspended", external_job_id)
+        if process_status in {"PSUSP", "USUSP", "SSUSP"}:
+            self.logger.debug("Job [%s] is suspended", external_job_id)
             suspended_info = "Job suspended"
             return Status.SUSPENDED, suspended_info.strip()
-        self.logger.debug(
-            "Job [%s] is in an unhandled state (%s)", external_job_id, process_status)
+        self.logger.debug("Job [%s] is in an unhandled state (%s)", external_job_id, process_status)
         status_info = "Job is in an unhandles state: {}".format(process_status)
         return Status.UNKNOWN, status_info.strip()
 
@@ -259,13 +245,13 @@ class LSFClient(object):
         bjobs_records = self.parse_bjobs(stdout)
         if bjobs_records:
             process_output = bjobs_records[0]
-            if 'STAT' in process_output:
-                process_status = process_output['STAT']
+            if "STAT" in process_output:
+                process_status = process_output["STAT"]
                 return self._handle_status(process_status, process_output, str(external_job_id))
-            if 'ERROR' in process_output:
+            if "ERROR" in process_output:
                 error_message = ""
-                if process_output['ERROR']:
-                    error_message = process_output['ERROR']
+                if process_output["ERROR"]:
+                    error_message = process_output["ERROR"]
                 return Status.UNKNOWN, error_message.strip()
         return None
 
@@ -278,10 +264,16 @@ class LSFClient(object):
         Returns:
             tuple: (Ridgeback Status int, extra info)
         """
-        bsub_command = ["bjobs", "-json", "-o",
-                        "user exit_code stat exit_reason pend_reason", str(external_job_id)]
+        bsub_command = [
+            "bjobs",
+            "-json",
+            "-o",
+            "user exit_code stat exit_reason pend_reason",
+            str(external_job_id),
+        ]
         self.logger.debug("Checking lsf status for job: %s", external_job_id)
-        process = subprocess.run(bsub_command, check=True, stdout=subprocess.PIPE,
-                                 universal_newlines=True)
+        process = subprocess.run(
+            bsub_command, check=True, stdout=subprocess.PIPE, universal_newlines=True
+        )
         status = self._parse_status(process.stdout, external_job_id)
         return status
