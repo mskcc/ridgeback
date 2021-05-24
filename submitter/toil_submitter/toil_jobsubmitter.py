f4066d17a1be683615ad736316aba81435d152c6
Ian <ionox0@gmail.com>
2021-05-24 14:47:05 -0400

    use main toil command for Nucleo (#209)
    * use main toil command for Nucleo
    
    * flake8 change
    
    * Update toil_jobsubmitter.py
    
    * Update toil_jobsubmitter.py
    
    * Update toil_jobsubmitter.py
    
    * update toil jobsubmitter for nucleo
    
    * remove whitespace

--- submitter/toil_submitter/toil_jobsubmitter.py
+++ submitter/toil_submitter/toil_jobsubmitter.py
@@ -46,6 +46,14 @@ class ToilJobSubmitter(JobSubmitter):
         toil_lsf_args = "-sla %s %s" % (settings.LSF_SLA, " ".join(self._job_args()))
         env["JAVA_HOME"] = None
         env["TOIL_LSF_ARGS"] = toil_lsf_args
+
+        if "access" in self.app.github.lower():
+            for e in ['SINGULARITYENV_SINGULARITY_DOCKER_USERNAME',
+                      'SINGULARITY_DOCKER_USERNAME',
+                      'SINGULARITYENV_SINGULARITY_DOCKER_PASSWORD',
+                      'SINGULARITY_DOCKER_PASSWORD']:
+                env[e] = None
+
         external_id = self.lsf_client.submit(
             command_line, self._job_args(), log_path, env
         )
@@ -156,7 +164,7 @@ class ToilJobSubmitter(JobSubmitter):
         return ["-M", self.memlimit] if self.memlimit else []
 
     def _command_line(self):
-        if "access" in self.app.github.lower():
+        if "access" in self.app.github.lower() and "nucleo" not in self.app.github.lower():
             """
             Start ACCESS-specific code
             """
