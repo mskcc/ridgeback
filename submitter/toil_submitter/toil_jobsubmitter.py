1e56634d25e467efadb095074d9e4236f5bce108
Ian <ionox0@gmail.com>
2021-09-24 10:38:45 -0400

    Use Complex Genotyping branch of ACCESS_pipeline
    This change will allow our SNV pipeline to better handle complex variants

--- submitter/toil_submitter/toil_jobsubmitter.py
+++ submitter/toil_submitter/toil_jobsubmitter.py
@@ -161,7 +161,7 @@ class ToilJobSubmitter(JobSubmitter):
             """
             Start ACCESS-specific code
             """
-            path = "PATH=/juno/home/accessbot/miniconda3/envs/ACCESS_2.0.0/bin:{}".format(os.environ.get("PATH"))
+            path = "PATH=/juno/home/accessbot/miniconda3/envs/ACCESS_cmplx_geno_test/bin:{}".format(os.environ.get("PATH"))
             command_line = [
                 path,
                 "toil-cwl-runner",
