528ca06b166d0421635b2cb4a85d219a1b0608b1
Nikhil Kumar <nikhilkumar516@gmail.com>
2023-11-09 14:57:57 -0500

    Added tests

--- tests/test_commandline.py
+++ tests/test_commandline.py
@@ -7,8 +7,7 @@ import tempfile
 from django.test import TestCase, override_settings
 import toil
 from orchestrator.models import Job, Status, PipelineType, CommandLineToolJob
-from orchestrator.tasks import check_status_of_command_line_jobs
-
+from orchestrator.tasks import check_status_of_command_line_jobs, check_job_hanging, check_leader_not_running
 
 class TestToil(TestCase):
     """
@@ -104,3 +103,146 @@ class TestToil(TestCase):
         mock_num_failed = 2
         num_failed = CommandLineToolJob.objects.filter(status=(Status.FAILED)).count()
         self.assertEqual(num_failed, mock_num_failed)
+    
+    def test_details_set(self):
+        """
+        Test if the metadata is being set for commandLineJObs
+        """
+        self.mock_track("running")
+        first_running_job = CommandLineToolJob.objects.filter(status=(Status.RUNNING)).first()
+        details = first_running_job.details
+        self.assertIsNotNone(details)
+        self.assertIsNotNone(details["cores_req"])
+        self.assertIsNotNone(details["cpu_usage"])
+        self.assertIsNotNone(details["job_stream"])
+        self.assertIsNotNone(details["last_modified"])
+        self.assertIsNotNone(details["log_path"])
+        self.assertIsNotNone(details["mem_usage"])
+        self.assertIsNotNone(details["memory_req"])
+    
+    def test_hanging_toil_leader_not_running(self):
+        """
+        Test detection of a hanging toil leader job before its running
+        """
+        self.mock_track("running")
+        self.job.status = Status.PENDING
+        self.job.save()
+        with override_settings(MAX_HANGING_HOURS=0):
+            check_leader_not_running()
+        self.job.refresh_from_db()
+        self.assertIsNotNone(self.job.message)
+        self.assertIsNotNone(self.job.message['alerts'][0])
+
+    def test_hanging_no_duplicated_alerts(self):
+        """
+        Test to make sure the same alert does not get triggered twice
+        """
+        self.mock_track("running")
+        self.job.status = Status.PENDING
+        self.job.save()
+        with override_settings(MAX_HANGING_HOURS=0):
+            check_leader_not_running()
+        self.job.refresh_from_db()
+        with override_settings(MAX_HANGING_HOURS=0):
+            check_leader_not_running()
+        self.job.refresh_from_db()
+        self.assertIsNotNone(self.job.message)
+        self.assertTrue(len(self.job.message['alerts']) == 1)
+
+    def test_hanging_toil_leader_running(self):
+        """
+        Test detection of a hanging toil leader job while its running
+        """
+        self.mock_track("running")
+        for single_job in CommandLineToolJob.objects.all():
+            single_job.status = Status.COMPLETED
+            single_job.save()
+        with override_settings(MAX_HANGING_HOURS=0):
+            check_job_hanging(self.job)
+        self.job.refresh_from_db()
+        self.assertIsNotNone(self.job.message)
+        self.assertIsNotNone(self.job.message['alerts'][0])
+
+    def test_hanging_toil_commandline_not_running(self):
+        """
+        Test detection of a hanging command that has not started yet
+        """
+        self.mock_track("running")
+        all_tool_jobs = CommandLineToolJob.objects.all()
+        count = all_tool_jobs.count()
+        for single_job in CommandLineToolJob.objects.all():
+            single_job.status = Status.PENDING
+            single_job.save()
+        with override_settings(MAX_HANGING_HOURS=0):
+            check_job_hanging(self.job)
+        self.job.refresh_from_db()
+        self.assertIsNotNone(self.job.message)
+        self.assertIsNotNone(self.job.message['alerts'][0])
+        self.assertTrue(len(self.job.message['alerts']) == count)
+
+    def test_hanging_toil_commandline_running(self):
+        """
+        Test detection of a hanging command that have started running
+        """
+        self.mock_track("running")
+        all_tool_jobs = CommandLineToolJob.objects.all()
+        count = all_tool_jobs.count()
+        for single_job in CommandLineToolJob.objects.all():
+            single_job.status = Status.RUNNING
+            single_job.save()
+        with override_settings(MAX_HANGING_HOURS=0):
+            check_job_hanging(self.job)
+        self.job.refresh_from_db()
+        self.assertIsNotNone(self.job.message)
+        self.assertIsNotNone(self.job.message['alerts'][0])
+        self.assertTrue(len(self.job.message['alerts']) == count)
+
+    def test_hanging_toil_commandline_mix(self):
+        """
+        Test detection of a hanging command that has not started yet and some that are running
+        """
+        self.mock_track("running")
+        first_command = CommandLineToolJob.objects.first()
+        first_command.status = Status.RUNNING
+        first_command.save()
+        last_command = CommandLineToolJob.objects.last()
+        last_command.status = Status.PENDING
+        last_command.save()
+        with override_settings(MAX_HANGING_HOURS=0):
+            check_job_hanging(self.job)
+        self.job.refresh_from_db()
+        self.assertIsNotNone(self.job.message)
+        self.assertIsNotNone(self.job.message['alerts'][0])
+    
+    def test_hanging_message_for_toil_leader_running(self):
+        """
+        Test alert sent of a hanging toil leader job while its running
+        """
+        self.mock_track("running")
+        MOCK_LOG_PATH = "path/to/log.log"
+        for single_job in CommandLineToolJob.objects.all():
+            single_job.status = Status.COMPLETED
+            single_job.save()
+        self.job.message['log'] = MOCK_LOG_PATH
+        self.job.save()
+        with override_settings(MAX_HANGING_HOURS=0):
+            check_job_hanging(self.job)
+        self.job.refresh_from_db()
+        self.assertEqual(self.job.message['log'],MOCK_LOG_PATH)
+        self.assertIsNotNone(self.job.message['alerts'][0])
+        self.assertTrue(MOCK_LOG_PATH in self.job.message['alerts'][0]['message'])
+
+    def test_hanging_message_for_toil_leader_running(self):
+        """
+        Test alert sent of a hanging tool while its running
+        """
+        self.mock_track("running")
+        first_command = CommandLineToolJob.objects.first()
+        first_command.status = Status.RUNNING
+        first_command.save()
+        command_log_path = first_command.details['log_path']
+        with override_settings(MAX_HANGING_HOURS=0):
+            check_job_hanging(self.job)
+        self.job.refresh_from_db()
+        self.assertIsNotNone(self.job.message['alerts'][0])
+        self.assertTrue(command_log_path in self.job.message['alerts'][0]['message'])
