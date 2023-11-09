e14e36c196b0d265577493638677bd49383f95bf
Nikhil Kumar <nikhilkumar516@gmail.com>
2023-11-09 14:59:14 -0500

    Added black formatting

--- orchestrator/tasks.py
+++ orchestrator/tasks.py
@@ -93,7 +93,6 @@ def process_jobs():
 
     check_leader_not_running.delay()
 
-
     for job_id in status_jobs:
         # Send CHECK_STATUS commands for Jobs
         command_processor.delay(Command(CommandType.CHECK_STATUS_ON_LSF, str(job_id)).to_dict())
@@ -107,7 +106,6 @@ def process_jobs():
         if Status(job.status).transition(Status.SUBMITTING):
             job.update_status(Status.SUBMITTING)
             command_processor.delay(Command(CommandType.SUBMIT, str(job.id)).to_dict())
-            
 
 
 @shared_task(bind=True)
@@ -259,38 +257,54 @@ def check_job_status(job):
     else:
         raise StopException("Invalid transition %s to %s" % (Status(job.status).name, Status(lsf_status).name))
 
-def _add_alert(job,alert_obj):
+
+def _add_alert(job, alert_obj):
     if "alerts" in job.message:
-        all_alerts = job.message['alerts']
-        alert_ons = [ single_alert["on"] for single_alert in all_alerts]
+        all_alerts = job.message["alerts"]
+        alert_ons = [single_alert["on"] for single_alert in all_alerts]
         current_alert_on = alert_obj["on"]
         if current_alert_on not in alert_ons:
             all_alerts.append(alert_obj)
-            job.message['alerts'] = all_alerts
+            job.message["alerts"] = all_alerts
             job.save()
     else:
-        job.message['alerts'] = [alert_obj]
+        job.message["alerts"] = [alert_obj]
         job.save()
+
 
 @shared_task(bind=True)
 def check_leader_not_running(self):
     logger.info("Checking for any jobs stuck in a non-running state")
     time_threshold = now() - timedelta(hours=int(settings.MAX_HANGING_HOURS))
-    jobs = Job.objects.filter(modified_date__lt=time_threshold).exclude(status__in=[Status.COMPLETED,Status.TERMINATED,Status.RUNNING])
+    jobs = Job.objects.filter(modified_date__lt=time_threshold).exclude(
+        status__in=[Status.COMPLETED, Status.TERMINATED, Status.RUNNING]
+    )
     for single_tool in jobs:
         status_name = Status(single_tool.status).name
         hang_time_seconds = (now() - single_tool.modified_date).total_seconds()
         hang_time_hours = divmod(hang_time_seconds, 3600)[0]
         message = "Leader job has been hanging on status {} for over {} hours.".format(status_name, hang_time_hours)
-        hang_time_obj = {"message": message, "hang_time":hang_time_hours, "since": str(single_tool.modified_date),"on":"leader_before_running"}
-        _add_alert(single_tool,hang_time_obj)
+        hang_time_obj = {
+            "message": message,
+            "hang_time": hang_time_hours,
+            "since": str(single_tool.modified_date),
+            "on": "leader_before_running",
+        }
+        _add_alert(single_tool, hang_time_obj)
+
 
 def check_job_hanging(single_running_job):
     time_threshold = now() - timedelta(hours=int(settings.MAX_HANGING_HOURS))
-    non_running_tools = CommandLineToolJob.objects.filter(modified_date__lt=time_threshold).exclude(status__in=[Status.COMPLETED,Status.TERMINATED,Status.RUNNING])
+    non_running_tools = CommandLineToolJob.objects.filter(modified_date__lt=time_threshold).exclude(
+        status__in=[Status.COMPLETED, Status.TERMINATED, Status.RUNNING]
+    )
     running_tools = CommandLineToolJob.objects.filter(root__id__exact=single_running_job.id, status=Status.RUNNING)
     if len(running_tools) == 0:
-        completed_jobs_finished_time = list(CommandLineToolJob.objects.filter(root__id__exact=single_running_job.id, status=Status.COMPLETED).exclude(finished__isnull=True).values_list('finished', flat=True))
+        completed_jobs_finished_time = list(
+            CommandLineToolJob.objects.filter(root__id__exact=single_running_job.id, status=Status.COMPLETED)
+            .exclude(finished__isnull=True)
+            .values_list("finished", flat=True)
+        )
         if completed_jobs_finished_time:
             latest_completed = max(completed_jobs_finished_time)
             if latest_completed < time_threshold:
@@ -301,21 +315,33 @@ def check_job_hanging(single_running_job):
                     log_file = single_running_job.message["log"]
                     if log_file:
                         message += " Check this log for more info: {}".format(log_file)
-                hang_time_obj = {"message": message, "hang_time":hang_time_hours, "since": str(latest_completed),"on":"leader_running"}
-                _add_alert(single_running_job,hang_time_obj)    
+                hang_time_obj = {
+                    "message": message,
+                    "hang_time": hang_time_hours,
+                    "since": str(latest_completed),
+                    "on": "leader_running",
+                }
+                _add_alert(single_running_job, hang_time_obj)
     for single_tool in non_running_tools:
         status_name = Status(single_tool.status).name
         hang_time_seconds = (now() - single_tool.modified_date).total_seconds()
         hang_time_hours = divmod(hang_time_seconds, 3600)[0]
         job_name = single_tool.job_name
         job_id = "{}_before_running".format(single_tool.job_id)
-        message = "{} job has been hanging on status {} for over {} hours.".format(job_name, status_name, hang_time_hours)
-        hang_time_obj = {"message": message, "hang_time":hang_time_hours, "since": str(single_tool.modified_date),"on":job_id}
-        _add_alert(single_running_job,hang_time_obj)
+        message = "{} job has been hanging on status {} for over {} hours.".format(
+            job_name, status_name, hang_time_hours
+        )
+        hang_time_obj = {
+            "message": message,
+            "hang_time": hang_time_hours,
+            "since": str(single_tool.modified_date),
+            "on": job_id,
+        }
+        _add_alert(single_running_job, hang_time_obj)
     for single_running_tool in running_tools:
         if "last_modified" not in single_running_tool.details:
             continue
-        modified_date_str = single_running_tool.details['last_modified']
+        modified_date_str = single_running_tool.details["last_modified"]
         if modified_date_str:
             modified_date = make_aware(dateparse.parse_datetime(modified_date_str))
             if modified_date < time_threshold:
@@ -323,15 +349,18 @@ def check_job_hanging(single_running_job):
                 hang_time_hours = divmod(hang_time_seconds, 3600)[0]
                 job_name = single_running_tool.job_name
                 job_id = "{}_running".format(single_running_tool.job_id)
-                message = "{} job has not logged an update for over {} hours.".format(job_name,hang_time_hours)
+                message = "{} job has not logged an update for over {} hours.".format(job_name, hang_time_hours)
                 if "log_path" in single_running_tool.details:
                     log_file = single_running_tool.details["log_path"]
                     if log_file:
                         message += " Check this log for more info: {}".format(log_file)
-                hang_time_obj = {"message": message, "hang_time":hang_time_hours, "since": str(modified_date),"on":job_id}
-                _add_alert(single_running_job,hang_time_obj)
-   
-
+                hang_time_obj = {
+                    "message": message,
+                    "hang_time": hang_time_hours,
+                    "since": str(modified_date),
+                    "on": job_id,
+                }
+                _add_alert(single_running_job, hang_time_obj)
 
 
 def terminate_job(job):
