058b21b9710589d9887f9471cb512369edf064df
Nikhil Kumar <nikhilkumar516@gmail.com>
2023-11-16 11:21:50 -0500

    Fix default walltime

--- orchestrator/models.py
+++ orchestrator/models.py
@@ -173,8 +173,8 @@ class Job(BaseModel):
     submitted = models.DateTimeField(blank=True, null=True)
     finished = models.DateTimeField(blank=True, null=True)
     track_cache = JSONField(blank=True, null=True)
-    leader_walltime = models.IntegerField(default=168)
-    tool_walltime = models.IntegerField(default=24)
+    leader_walltime = models.IntegerField(default=7200)
+    tool_walltime = models.IntegerField(default=1440)
     memlimit = models.CharField(blank=True, null=True, default=None, max_length=20)
     metadata = JSONField(blank=True, null=True, default=dict)
 
