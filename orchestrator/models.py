b5e3fbd82526aa2e27a6cf295d8cc310800d09d9
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2025-03-28 10:41:00 -0400

    Allow job_prefix to be blank

--- orchestrator/models.py
+++ orchestrator/models.py
@@ -173,7 +173,7 @@ class Job(BaseModel):
     resume_job_store_location = models.CharField(max_length=1000, null=True, blank=True)
     working_dir = models.CharField(max_length=1000, null=True, blank=True)
     log_dir = models.CharField(max_length=1000, null=True, blank=True)
-    log_prefix = models.CharField(max_length=100, default="")
+    log_prefix = models.CharField(max_length=100, default="", blank=True)
     status = models.IntegerField(
         choices=[(status.value, status.name) for status in Status],
         default=Status.CREATED,
