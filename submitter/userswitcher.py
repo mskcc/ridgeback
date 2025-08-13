2180681ca772c553af8dc0127352f816d4810caa
Nikhil Kumar <nikhilkumar516@gmail.com>
2025-08-13 12:37:41 -0400

    Update user script for user specific functions

--- submitter/userswitcher.py
+++ submitter/userswitcher.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import marshal
 import subprocess
@@ -8,25 +9,41 @@ import logging
 from pathlib import Path
 from functools import wraps
 from getpass import getuser
+import django
+import tempfile
+import json
 from django.conf import settings
 
 log = logging.getLogger(__name__)
 
 
 def userscript():
+    
+    ridgeback_path = os.environ.get("RIDGEBACK_PATH","/usr/bin/ridgeback")
+    sys.path.append(ridgeback_path)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ridgeback.settings")
     stdout_buffer = io.StringIO()
     stderr_buffer = io.StringIO()
     exception_raised = False
+    output = None
     with contextlib.redirect_stdout(stdout_buffer), contextlib.redirect_stderr(stderr_buffer):
         try:
-            func_serialized, args, kwargs = marshal.loads(sys.stdin.buffer.read())
-            func = dill.loads(func_serialized)
+            env_str = sys.argv[1]
+            env_json = json.loads(env_str)
+            for single_env in env_json:
+                if single_env=='PATH':
+                    os.environ[single_env] = env_json[single_env]
+                else:
+                    os.environ.setdefault(single_env,env_json[single_env])
+            django.setup()
+            func_data = sys.stdin.buffer.read()
+            func, args, kwargs = dill.loads(func_data)
             output = func(*args, **kwargs)
         except Exception:
             log.exception("Exception when running the function as another user")
             exception_raised = True
-    script_tuple = (output, stdout_buffer.getvalue())
-    sys.stderr.buffer.write(stderr_buffer.getvalue())
+    script_tuple = (output, stdout_buffer.getvalue().encode())
+    sys.stderr.buffer.write(stderr_buffer.getvalue().encode())
     sys.stdout.buffer.write(dill.dumps(script_tuple))
     if exception_raised:
         sys.exit(1)
@@ -37,25 +54,44 @@ def userswitch(func):
     def dzdo_wrapper(*args, **kwargs):
         # jobsubmitter/batchsystem objects will have the user attribute in self
         user = args[0].user
+        current_env = {}
         if user == getuser() or not settings.ENABLE_USER_SWITCH:
             return func(*args, **kwargs)
         else:
-            proc_command = ["dzdo", "-u", f"{user}", sys.executable, "-c", Path(__file__).absolute()]
-            serialized_func = dill.dumps(func)
-            func_data = marshal.dumps((serialized_func, args, kwargs))
+            for key, value in os.environ.items():
+                current_env[key] = value
+            proc_command = ["dzdo", "--login", "-u", f"{user}", sys.executable, Path(__file__).absolute()]
         try:
-            dzdo_process = subprocess.run(proc_command, input=func_data, check=True, capture_output=True)
-            log.error(dzdo_process.stderr)
+            job_func = dill.dumps((func, args, kwargs))
+            env_str = json.dumps(current_env)
+            dzdo_process = subprocess.run(proc_command+[env_str], input=job_func, check=True, capture_output=True, env=current_env)
             output, stdout = dill.loads(dzdo_process.stdout)
-            log.info(stdout)
+            func_stdout = stdout.decode().strip()
+            func_stderr = dzdo_process.stderr.decode().strip()
+            if func_stdout:
+                log.info(func_stdout)
+            if func_stderr:
+                log.info(func_stderr)
+            log.error(func_stderr)
             return output
         except subprocess.CalledProcessError as e:
+            stdout_str = ""
+            stderr_str = ""
+            try:
+                output, stdout = dill.loads(e.output)
+                if stdout:
+                    stdout_str = stdout.decode().strip()
+                stderr = e.stderr
+                if stderr:
+                    stderr_str = stderr.decode().strip()
+            except:
+                stdout_str = "NA"
             exception_message = f"""
             Error while userswitching:
-            Command: {e.cmd}
+            USER: {user}
             Return Code: {e.returncode}
-            Output: {e.output}
-            Error: {e.stderr}
+            Output: {stdout_str}
+            Error: {stderr_str}
             """
             raise Exception(exception_message)
         except FileNotFoundError as e:
