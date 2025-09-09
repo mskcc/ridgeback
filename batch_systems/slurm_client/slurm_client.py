8d400ef646d0afcd4bebd1276c62399ce54418b1
buehlere <buehlere@mskcc.org>
2025-09-09 14:01:31 -0400

    Update slurm_client.py

--- batch_systems/slurm_client/slurm_client.py
+++ batch_systems/slurm_client/slurm_client.py
@@ -69,6 +69,7 @@ class SLURMClient(BatchClient):
             sbatch_command,
             check=True,
             stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
             universal_newlines=True,
             cwd=work_dir,
             env=current_env,
